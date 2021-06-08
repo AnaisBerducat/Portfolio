7c9317e0faeddf0613f1c60cba42773eee223bfa
Anais <anais.berducat@outlook.fr>
2021-06-08 15:15:58 +0200

    Version web terminée

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php include 'navbar.php' ?>
+<body>
+
+<div class="background-1">
+    <div class="introduction">
+        <img class="profil" src="Assets/Profil.jpg">
+        <div class="title">
+        <h1 class="center">BERDUCAT Anaïs</h1>
+        <h2 class="center">Développeuse Web Junior PHP / Symfony</h2>
+        </div>
+    </div>
+</div>
+
+<div class="background-2" style="display: flex;">
+    <div class="padding" style="padding-top: 5%;" id="ancre1">
+    <h4>A propos de moi</h4>
+    <p class="presentation">Possédant un Baccalauréat dans le domaine du Social, j'ai souhaité effectuer une reconversion dans le domaine de l'informatique
+        afin de pouvoir m'épanouir pleinement dans mon métier futur. Le développement web m'intéressait depuis quelques années et j'ai 
+        décidée d'intégrer la formation Développeur Web PHP de la Wild Code School à distance.
+        Actuellement en fin de formation, je suis à la recherche d'une <strong>alternance Web & Web Mobile</strong> sur Tours (37) ou à distance uniquement.<br><br>
+        <strong>Stack technique - PHP, Symfony, Mysql, Bootstrap, Git, Linux</strong>
+    </p>
+    </div>
+    <img class="code" src="Assets/Code.jpg">
+</div>
+
+<div class="background-1" id="ancre2">
+<h4 class="text-center">Mes Projets</h4>
+    <div class="img-left">
+    <img src="Assets/VapHot.png" class="projects">
+        <div class="text">
+        <h4 class="center"><strong>VapHot</strong></h4>
+        <h5>Réalisé en 2 semaines en équipe de 2 personnes</h5>
+        <p>Projet réalisé en HTML/CSS, MySQL, PHP.<br>
+        Site E-Commerce de vente de cigarettes électronique.
+        </p>
+        </div>
+    </div>
+    <div class="img-right">
+        <div class="text">
+    <h4 class="center"><strong>BombHackaton</strong></h4>
+    <h5>Réalisé en 24 heures en équipe de 5 personnes</h5>
+    <p>Projet réalisé en HTML/CSS, MySQL, PHP.<br>
+    Site permettant à des marsiens solitaires de rencontrer des personnes de différentes planètes.
+    Possibilité de voir les offres de voyages à travers les planètes.
+    </p>
+        </div>
+    <img src="Assets/BombHackaton.png" class="projects">
+    </div>
+</div>
+<div class="background-2" id="ancre3">
+    <h4 class="text-center">Mon CV & mes Coordonnées</h4>
+    <div class="cv">
+    <a href="Assets/BERDUCAT_Anaïs_DeveloppeurWebPHP.pdf" class="download" target="_blank"><strong>Télécharger le PDF</strong></a>
+    <i class="fas fa-file-download" style="color:black"></i>
+    </div><br>
+    <div class="social" id="ancre4">
+    <a href="https://www.linkedin.com/in/ana%C3%AFs-berducat-a77455208/" style="margin-right: 1%;"><i class="fab fa-linkedin" style="color: black;"></i></a>
+    <a href="https://github.com/AnaisBerducat"><i class="fab fa-github" style="color: black;"></i></a>
+    </div>
+</div>
+
+
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="Assets/style.css" />
+    <script src="https://kit.fontawesome.com/2d0e64c378.js" crossorigin="anonymous"></script>
+
+    <title>Anaïs BERDUCAT - Développeuse WEB PHP / Symfony</title>
+
+    <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0 mx-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page">BERDUCAT Anaïs</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#ancre1">Présentation</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#ancre2">Mes Projets</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#ancre3">Mon CV</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#ancre4">Mes coordonnées</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</head>
